127486cff60ef5cc772d32f40be94f6f57eb7943
Tsquare25 <tibebe1234t@gmail.com>
2021-10-06 14:52:41 +0100

    made react handle all the routs

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
+from django.urls.conf import re_path
 from .views import index
 urlpatterns = [
-    path("", index),
-    path("join/", index),
-    path("create/", index),
-    path("room/<str:roomCode>", index),
-]
+    re_path("", index)
+] 
