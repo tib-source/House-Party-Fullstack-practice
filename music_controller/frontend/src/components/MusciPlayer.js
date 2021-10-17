318fe44ba443ae9898cfec821745771a1a0ccdfd
Tsquare25 <tibebe1234t@gmail.com>
2021-10-17 18:57:39 +0100

    fixed music component styling

--- music_controller/frontend/src/components/MusciPlayer.js
+++ music_controller/frontend/src/components/MusciPlayer.js
@@ -29,33 +29,22 @@ export default class MusicPlayer extends Component {
     const songProgress = (this.props.time / this.props.duration) * 100;
     return (
       <Card>
-        <Grid container alignitems="center">
-          <Grid item xs={4} align="center">
-            <img
-              src={this.props.image_url}
-              heigh="100%"
-              width="100%"
-              alt="album cover"
-            />
+        <Grid container alignItems="center">
+          <Grid item align="center" xs={4}>
+            <img src={this.props.image_url} height="100%" width="100%" />
           </Grid>
-          <Grid item xs={8} align="center">
+          <Grid item align="center" xs={8}>
             <Typography component="h5" variant="h5">
               {this.props.title}
             </Typography>
-            <Typography
-              color="textSecondary"
-              component="h5"
-              variant="subtitle1"
-            >
+            <Typography color="textSecondary" variant="subtitle1">
               {this.props.artist}
             </Typography>
             <div>
               <IconButton
-                onClick={
-                  this.props.is_playing
-                    ? this.alterSong("pause")
-                    : this.alterSong("play")
-                }
+                onClick={() => {
+                  this.props.is_playing ? this.pauseSong() : this.playSong();
+                }}
               >
                 {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
               </IconButton>
