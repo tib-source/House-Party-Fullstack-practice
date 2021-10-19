75c41383cd112b5024f2e56dcd53f677bbe0ef12
Tsquare25 <tibebe1234t@gmail.com>
2021-10-19 10:56:47 +0100

    added song_id in model

--- music_controller/spotify/models.py
+++ music_controller/spotify/models.py
@@ -16,5 +16,5 @@ class SpotifyToken(models.Model):
 class Votes(models.Model): 
   user = models.CharField(max_length=50, unique=True)
   created_at = models.DateField(auto_now_add=True)
-  song_id = models.CharField(max_length=50)
+  song_id = models.CharField(max_length=50, )
   room = models.ForeignKey(Room, on_delete=models.CASCADE)
